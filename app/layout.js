a1d5bd2517989c20cb5fd049d0b5655bfe5f45c5
Sanjana-Anandan <sanjana.anandan17.com>
2025-08-06 22:15:47 +0530

    updated layout page

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
            <main className="min-h-screen">{children}</main>
            <footer className="bg-muted/50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Made by Sanjana</p>
+              <p>Made by Sanjana Anandan</p>
             </div>
            </footer>
           </ThemeProvider>
