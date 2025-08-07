30f4b49bb114c520a6c802e15d83ff4e49d162f9
Sanjana Anandan <137036596+Sanjana-Anandan@users.noreply.github.com>
2025-08-07 22:22:23 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Button } from "../components/ui/button";
 export default function Home() {
   return (
-    <div>Welcome to CareerMate! Your personalised AI Career Guide!
+    <div>Welcome to Career Mate! Your personalised AI Career Guide!
     <br/>
     <Button>Hello</Button>
     </div>
